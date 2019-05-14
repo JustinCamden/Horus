a72f78698c4cad0f733afce42c4068008afb8a22
Unknown <camden@usc.edu>
2019-05-13 23:38:17 -0700

    Adding project.

---
+++ Source/Horus/HorusGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HorusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORUS_API AHorusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
